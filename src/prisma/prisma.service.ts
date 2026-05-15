ea1620e35e76fb9e2de531aaece0a25127a5fe0a
fauzannurhikmah <fauzannurhikmah0@gmail.com>
2026-05-15 16:36:49 +0700

    Add global API prefix and refactor PrismaService constructor

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,18 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  OnModuleInit,
+} from '@nestjs/common';
+
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit
-{
+  implements OnModuleInit {
+  constructor() {
+    super({});
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
